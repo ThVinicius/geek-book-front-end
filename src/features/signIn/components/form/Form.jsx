9f99f49ce98e807f9b1829bce8a50090be84b889
Vinicius Pacheco <vinicioss9955@gmail.com>
2022-09-28 13:59:01 -0300

    page signIn completed

--- src/features/signIn/components/form/Form.jsx
+++ src/features/signIn/components/form/Form.jsx
@@ -1,18 +1,41 @@
+import { useState } from 'react'
+import useApi from '../../../../hooks/useApi'
+import request from '../../api/request'
+import useToast from '../../../../hooks/useToast'
 import InputPassword from '../../../../components/inputPassword/Input'
 import Input from '../../../../components/input/Input'
 import SubmitButton from '../../../../components/loadingButton/Button'
 import { Container, Anchor } from './formStyles'
 
 export default function Form() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [response, fetch] = useApi()
+
+  useToast(response)
+
   function submit(event) {
     event.preventDefault()
+
+    const data = { email, password }
+
+    fetch(...request(data))
   }
 
   return (
     <Container onSubmit={submit}>
       <h1>Login</h1>
-      <Input label="Email" type="email" />
-      <InputPassword label="Senha" />
+      <Input
+        label="Email"
+        type="email"
+        value={email}
+        onChange={e => setEmail(e.target.value)}
+      />
+      <InputPassword
+        label="Senha"
+        value={password}
+        onChange={e => setPassword(e.target.value)}
+      />
       <SubmitButton name="Logar" />
       <Anchor to="/signup">Não possuo cadastro</Anchor>
     </Container>

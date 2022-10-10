87522954a7b302c9e64366905cc68ba0b1e99377
Vinicius Pacheco <vinicioss9955@gmail.com>
2022-10-10 17:48:37 -0300

    login persistence

--- src/features/signIn/pages/SignIn.jsx
+++ src/features/signIn/pages/SignIn.jsx
@@ -1,8 +1,27 @@
-import AppContainer from '../../../containers/auth/App'
-import Content from '../../../components/authContent/Content'
-import Form from '../components/form/Form'
+import { useEffect } from "react"
+import { useNavigate } from "react-router-dom"
+import { useGlobal } from "../../../context/globalContext"
+import AppContainer from "../../../containers/auth/App"
+import Content from "../../../components/authContent/Content"
+import Form from "../components/form/Form"
 
 export default function SignIn() {
+  const { global } = useGlobal()
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const userStringfy = localStorage.getItem("user")
+
+    if (userStringfy !== null) {
+      const { token, ...user } = JSON.parse(userStringfy)
+
+      global.token = token
+      global.user = user
+
+      navigate("/home")
+    }
+  }, [])
+
   return (
     <AppContainer>
       <Content>

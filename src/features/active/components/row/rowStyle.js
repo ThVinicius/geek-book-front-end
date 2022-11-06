38ff027e4086bed937e8c4d29b26cff0fd3be1ac
ThVinicius <vinicioss9955@gmail.com>
2022-11-06 18:36:33 -0300

    style: padding

--- src/features/active/components/row/rowStyle.js
+++ src/features/active/components/row/rowStyle.js
@@ -1,14 +1,14 @@
-import styled from "styled-components"
+import styled from 'styled-components'
 
 const H6 = styled.h6`
-  padding: 9px 0px;
+  padding: 9px;
   background-color: #808080;
   color: #ffffff;
   display: flex;
   align-items: center;
   justify-content: center;
   border-radius: 5px;
-  font: normal 400 18px "Lexend", sans-serif;
+  font: normal 400 18px 'Lexend', sans-serif;
 `
 
 export { H6 }
